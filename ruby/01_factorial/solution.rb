db16645a77cb3e630d766cbc4948d51d75fea349
xmas-alexandr <alexsandr.xmas@gmail.com>
2018-07-11 22:29:38 +0400

    new commit 3

--- ruby/01_factorial/solution.rb
+++ ruby/01_factorial/solution.rb
@@ -1,11 +1,13 @@
 def factorial(n)
-if n == 0 || n == 1 
+if ((n.is_a? Integer == false) || n < 0)
+raise RuntimeError
+elsif n == 0 || n == 1 
      1
 else
-sum = 1
+    sum = 1
     n.times do |t|
       sum *= (t + 1)
     end
     sum
 end
-end
+en
