22784068f9b1340b34db39db3026fb0bcd2f292e
xmas-alexandr <alexsandr.xmas@gmail.com>
2018-07-11 22:56:17 +0400

    Work

--- ruby/01_factorial/solution.rb
+++ ruby/01_factorial/solution.rb
@@ -1,5 +1,5 @@
 def factorial(n)
-if ((n.is_a? Integer == false) || n < 0)
+if ((n.is_a?(Integer) == false) || n < 0)
 raise RuntimeError
 elsif n == 0 || n == 1 
      1
@@ -10,4 +10,4 @@ else
     end
     sum
 end
-en
+end
